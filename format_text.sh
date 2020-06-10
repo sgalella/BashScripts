9b0f65f37eb024e3bde879c1340ac3018e08ae58
sgalella <santi.galella@gmail.com>
2020-06-10 15:18:19 +0200

    Add format_text

---
+++ format_text.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get filename
+IN=$1
+
+# Check if output file exists
+OUT="out.txt"
+if [ -d $OUT ]
+then
+	rm "out.txt"
+fi
+
+# Add additional newline at the end (for reading purposes)
+echo "" >> $IN
+
+# Append newline to ouput file
+s=""
+while IFS= read -r  line 
+do
+	printf "%s " $line | tr -d '\n' >> $OUT
+done < $IN
+
